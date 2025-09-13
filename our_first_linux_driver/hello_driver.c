7c034949c5a97d324214e2b8ab6015b9cee5c75d
Zuhaitz <zuhaitz.zechhub@gmail.com>
2025-09-13 11:17:03 +0100

    Added driver example

---
+++ our_first_linux_driver/hello_driver.c
@@ -0,0 +1,36 @@
+/*
+ * Load the driver:
+ * sudo insmod hello_driver.ko
+ *
+ * Check the kernel log for your message:
+ * dmesg | tail
+ *
+ * Unload the driver:
+ * sudo rmmod hello_driver
+ *
+ * Check the log again:
+ * dmesg | tail 
+ */
+
+#include <linux/init.h>
+#include <linux/module.h>
+#include <linux/kernel.h>
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("Zuhaitz :3");
+MODULE_DESCRIPTION("A simple Hello World kernel module.");
+MODULE_VERSION("0.1");
+
+static int __init hello_init(void)
+{
+    printk(KERN_INFO "Hello, World! Driver loaded.\n");
+    return 0;
+}
+
+static void __exit hello_exit(void)
+{
+    printk(KERN_INFO "Goodbye, World! Driver unloaded.\n");
+}
+
+module_init(hello_init);
+module_exit(hello_exit);
